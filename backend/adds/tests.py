2f8e3364501901eba82f65773cec1fcd2eeaab0d
Oskar <oskar_rosenqvist@hotmail.com>
2023-03-14 16:41:38 +0100

    automated tests and github actions

--- backend/adds/tests.py
+++ backend/adds/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import Add
+from core.tests import UserFactory, PlocketUser
 
-# Create your tests here.
+
+class AddFactory:
+    @classmethod
+    def create_testadds(self, number):
+        if not number:
+            number = 1
+
+        for i in range(number):
+            add, _ = Add.objects.get_or_create(
+                user=PlocketUser.objects.get(id=i+1),
+            title=f'Add {i+1}',
+            price=100,
+            description=f'This is add nr {i+1}'
+            )
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.number=2
+        UserFactory.create_testusers(self.number)
+        AddFactory.create_testadds(self.number)
+
+
+class TestModel(TestCase):
+
+    def setUp(self):
+        pass
+
+
+class AddTestView(TestView):
+
+    def test_get_adds(self):
+        response = self.client.get('/api/v1/adds/')
+        self.assertEqual(response.data.get('results')[0].get('title'), 'Add 1')
+        self.assertEqual(len(response.data.get('results')), 2)
--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import PlocketUser
 
-# Create your tests here.
+
+class UserFactory:
+    @classmethod
+    def create_testusers(self, number):
+        if not number:
+            number = 1
+
+        for i in range(number):
+            user, _ = PlocketUser.objects.get_or_create(
+                username=f'test_user{i}',
+                first_name='test',
+                last_name=f'user{i}',
+                email=f'test_user{i}@test.com',
+            )
+
+
+class TestView(TestCase):
+    def setUp(self):
+        pass
+
+
+class TestModel(TestCase):
+
+    def setUp(self):
+        pass
